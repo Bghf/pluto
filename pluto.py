19ccad4c05b24e2e274639edb0ce2a885a57a983
Behzad <bzd@osx.home>
2018-12-05 14:46:10 +0000

    move forward and backward implemented

--- pluto.py
+++ pluto.py
@@ -1,11 +1,31 @@
+import unittest
 from unittest import TestCase
-
-print('ok')
 
 
 class Rover:
-  pass
+  def __init__(self):
+    self.x = 0
+    self.y = 0
 
+  def move_forward(self):
+    self.y = self.y + 1
+
+  def move_backward(self):
+    self.y = self.y - 1
+
+  def get_state(self):
+    return [self.x, self.y]
 
 class TestRover(TestCase) :
-  pass
+  def test_move_forward_increments_y(self):
+    rover = Rover()
+    rover.move_forward()
+    self.assertEqual(rover.get_state() ,[0, 1])
+
+  def test_move_backward_decrements_y(self):
+      rover = Rover()
+      rover.move_backward()
+      self.assertEqual(rover.get_state() ,[0, -1])
+
+if __name__ == '__main__':
+    unittest.main()
